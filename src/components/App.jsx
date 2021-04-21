c75e8f88810870ec9d65ea3b15506b7ceca6cc9f
Kevin Cotrina <kcotrinma92@gmail.com>
2021-04-21 00:37:27 -0500

    Add cibtact form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,64 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { useState } from 'react';
 
 const App = ({ ADD_CONTACT, contacts }) => {
-	const showProps = () => {
-		console.log(contacts);
+	const [formStates, setFormStates] = useState({
+		name: '',
+		lastname: '',
+		phone: '',
+		email: '',
+	});
+
+	const handleChange = ({ target }) => {
+		setFormStates((prev) => ({
+			...prev,
+			[target.name]: target.value,
+		}));
 	};
 
 	return (
 		<div>
 			<h1>To-Do list</h1>
-			<button
-				onClick={() =>
-					ADD_CONTACT({
-						type: 'ADD_CONTACT',
-						payload: { name: 'kevin', lastname: 'cotrina' },
-					})
-				}>
-				Add contact
-			</button>
-			<button onClick={showProps}>show Props</button>
+			<form action=''>
+				<label>
+					Name
+					<input value={formStates.name} name='name' onChange={handleChange} />
+				</label>
+				<label>
+					Lastname
+					<input
+						value={formStates.lastname}
+						name='lastname'
+						onChange={handleChange}
+					/>
+				</label>
+				<label>
+					phone
+					<input
+						value={formStates.phone}
+						name='phone'
+						onChange={handleChange}
+					/>
+				</label>
+				<label>
+					E-mail
+					<input
+						value={formStates.email}
+						name='email'
+						onChange={handleChange}
+					/>
+				</label>
+				<button
+					onClick={() =>
+						ADD_CONTACT({
+							type: 'ADD_CONTACT',
+							payload: { name: 'kevin', lastname: 'cotrina' },
+						})
+					}>
+					Add contact
+				</button>
+			</form>
 		</div>
 	);
 };
