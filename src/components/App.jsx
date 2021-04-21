d03f0e838fa2bde6125305d0e944b8c118b9f008
Kevin Cotrina <kcotrinma92@gmail.com>
2021-04-21 00:18:09 -0500

    Add dispatch and mapstateprops

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,38 @@
-const App = () => <h1>Hello World</h1>;
+import React from 'react';
+import { connect } from 'react-redux';
 
-export default App;
+const App = ({ ADD_CONTACT, contacts }) => {
+	const showProps = () => {
+		console.log(contacts);
+	};
+
+	return (
+		<div>
+			<h1>To-Do list</h1>
+			<button
+				onClick={() =>
+					ADD_CONTACT({
+						type: 'ADD_CONTACT',
+						payload: { name: 'kevin', lastname: 'cotrina' },
+					})
+				}>
+				Add contact
+			</button>
+			<button onClick={showProps}>show Props</button>
+		</div>
+	);
+};
+
+const mapStateToProps = (state) => ({
+	contacts: state.contactsReducer,
+});
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		ADD_CONTACT: (data) => {
+			dispatch({ type: 'ADD_CONTACT', payload: data });
+		},
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/reducers/contactsReducer.js
+++ src/reducers/contactsReducer.js
@@ -1,12 +1,23 @@
-const INITIAL_STATE = {
-	name: '',
-	lastname: '',
-	phone: '',
-	email: '',
-};
+const INITIAL_STATE = [
+	{
+		name: 'kevin',
+		lastname: 'cotrina',
+		phone: '555555555',
+		email: 'kevin@cotrina.com',
+	},
+	{
+		name: 'michael',
+		lastname: 'marquez',
+		phone: '222222222',
+		email: 'michael@marquez.com',
+	},
+];
 
 const contactsReducer = (state = INITIAL_STATE, action) => {
 	switch (action.type) {
+		case 'ADD_CONTACT':
+			console.log(action);
+			return state;
 		default:
 			return state;
 	}
